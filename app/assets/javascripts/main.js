c6570d7f9d65c4fac68841ca95bb7e5c59df7267
Wonsup Lee <daul0212@gmail.com>
2013-06-20 12:13:46 +0900

    clippy를 zeroClipboard로 교체

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    var clip = new ZeroClipboard($(".copy-button"), {
+      moviePath: "ZeroClipboard.swf"
+    });
+
+    clip.on('load', function (client) {
+      debugstr("Flash movie loaded and ready.");
+    });
+
+    clip.on('noFlash', function (client) {
+      $(".demo-area").hide();
+      debugstr("Your browser has no Flash.");
+    });
+
+    clip.on('wrongFlash', function (client, args) {
+      $(".demo-area").hide();
+      debugstr("Flash 10.0.0+ is required but you are running Flash " + args.flashVersion.replace(/,/g, "."));
+    });
+
+    clip.on('complete', function (client, args) {
+      debugstr("Copied text to clipboard: " + args.text);
+    });
+
+    // jquery stuff (optional)
+    function debugstr(text) {
+      $("#d_debug").append($("<p>").text(text));
+    }
+
+    $("#clear-test").on("click", function(){
+      $("#fe_text").val("Copy me!");
+      $("#testarea").val("");
+    });
+  });
